082b0034a8471d59eeacfe4a2ed3da56cec93681
Krishna <libraryismyparadise@gmail.com>
2022-06-29 02:47:34 +0545

    Add remove method

--- app.js
+++ app.js
@@ -8,21 +8,14 @@ class AwesomeBook {
 }
 
 class DynamicHtml {
-  constructor(){
-    return [];
-  }
-  addBook(book) {
-    this.push(book);
-  }
-  displayElements() {
-    console.log(this);
-    this.forEach((book, index) => this.addBookToTable(book, index));
+  static displayElements(booksCollection) {
+    booksCollection.forEach((book, index) => DynamicHtml.addBookToTable(book, index));
   }
 
-  addBookToTable(book, id) {
-    console.log(this);
+  static addBookToTable(book, id) {
     const bookList = document.querySelector('#book-list');
     const bookRecord = document.createElement('tbody');
+
     const titleCell = document.createElement('tr');
     const authCell = document.createElement('tr');
     const horline = document.createElement('hr');
@@ -33,34 +26,43 @@ class DynamicHtml {
     removeBtn.id = id;
     removeBtn.className = 'removeBtn';
     removeBtn.textContent = 'Remove';
-    removeBtn.onclick = removeBook;
     bookRecord.append(titleCell, authCell, removeBtn, horline);
     bookList.appendChild(bookRecord);
   }
+
+  static removeBook() {
+    const author = document.getElementById('author');
+    const id = JSON.parse(localStorage.getItem(booksCollection));
+    booksCollection.forEach((book, index) => {
+      if (id.author === author) {
+        id.splice(index, 1);
+      }
+    });
+  }
 }
 
-function removeBook(event) {
-  const id = parseInt(event.target.id, 10);
-  booksCollection.splice(id,1);
-  localStorage.setItem('booksCollection', JSON.stringify(booksCollection));
-  event.target.parentNode.remove();
-}
+document.querySelector('.removeBtn').addEventListener('click', (event) => {
+  DynamicHtml.removeBook(event.target);
+});
 
 const addBtn = document.getElementById('book-form');
-let booksCollection = new DynamicHtml();
-if (localStorage.getItem('booksCollection')) booksCollection = JSON.parse(localStorage.getItem('booksCollection'));
+let booksCollection = [];
+if (localStorage.getItem('booksCollection')) {
+  booksCollection = JSON.parse(localStorage.getItem('booksCollection'));
+}
 addBtn.addEventListener('submit', () => {
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
   const book = new AwesomeBook(title, author);
-  booksCollection.addBook(book);
-  console.log(booksCollection);
+  booksCollection.push(book);
   const id = booksCollection.length - 1;
-  booksCollection.addBookToTable(book, id);
+  DynamicHtml.addBookToTable(book, id);
   localStorage.setItem('booksCollection', JSON.stringify(booksCollection));
 });
 
 window.addEventListener('load', () => {
-  // if (localStorage.getItem('booksCollection')) booksCollection = JSON.parse(localStorage.getItem('booksCollection'));
-  // booksCollection.displayElements;
+  if (localStorage.getItem('booksCollection')) {
+    booksCollection = JSON.parse(localStorage.getItem('booksCollection'));
+  }
+  DynamicHtml.displayElements(booksCollection);
 });
