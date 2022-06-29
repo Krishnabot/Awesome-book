638307b2c377badd66c2f9b0353dd03d581711af
Jafer Yousef <jaferll@gmail.com>
2022-06-29 21:17:38 +0300

    Add final styling for feature3

--- app.js
+++ app.js
@@ -60,6 +60,7 @@ window.addEventListener('load', () => {
     booksCollection = JSON.parse(localStorage.getItem('booksCollection'));
   }
   DynamicHtml.displayElements(booksCollection);
+  document.getElementById('listLink').click();
 });
 
 const navLinks = document.querySelectorAll('a');
@@ -67,18 +68,24 @@ const listSection = document.getElementById('booksList');
 const newForm = document.getElementById('book-form');
 const contactInfo = document.getElementById('contact');
 function nav() {
+  navLinks.forEach((link) => {
+    link.classList.remove('active');
+  });
   if (this.id === 'listLink') {
     listSection.className = '';
     newForm.className = 'hide';
     contactInfo.className = 'hide';
+    this.classList.add('active');
   } else if (this.id === 'newLink') {
     listSection.className = 'hide';
     newForm.className = '';
     contactInfo.className = 'hide';
+    this.classList.add('active');
   } else {
     listSection.className = 'hide';
     newForm.className = 'hide';
     contactInfo.className = '';
+    this.classList.add('active');
   }
 }
 
